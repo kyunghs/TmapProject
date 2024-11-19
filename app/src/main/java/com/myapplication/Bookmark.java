2933a6f53b189a3080a8966e8c67f37cde48a0c5
ㅏããkoyj115 <koyj11525@gmail.com>
2024-11-20 00:33:38 +0900

    2024.11.19 - 고영진 : 커밋 최신화 (즐겨찾기 리스트, 경로 선택, 길안내 하단 시트, 길안내 종료 화면 xml 추가 및 fragment_home.xml, 주차장 정보 팝업, 남은 자리수 예측 UI 수정)

---
+++ app/src/main/java/com/myapplication/Bookmark.java
@@ -0,0 +1,19 @@
+package com.myapplication;
+
+public class Bookmark {
+    private String title;
+    private String subtitle;
+
+    public Bookmark(String title, String subtitle) {
+        this.title = title;
+        this.subtitle = subtitle;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getSubtitle() {
+        return subtitle;
+    }
+}
