06a109950d572e0d71c35d2f69abad829de97aed
kyunghs <gurtnrud@gmail.com>
2024-11-19 22:41:41 +0900

    2024.11.19 - 경혁수 : Flask 관련 정보 수정

--- FlaskServer/main.py
+++ FlaskServer/main.py
@@ -156,4 +156,4 @@ def predict_endpoint():
     return jsonify(result)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8080)
+    app.run(debug=True, host='0.0.0.0', port=8241)
