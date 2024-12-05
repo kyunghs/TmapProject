4b598c97eb0691a22f68cbf9f9c3fc854fcc759a
Administrator <rudgurtn@naver.com>
2024-12-05 19:28:38 +0900

    24.12.05 - 경혁수 : 주차장 response 데이터 추가

--- FlaskServer/db_query.py
+++ FlaskServer/db_query.py
@@ -273,7 +273,7 @@ def getParkInfo(u_lat, u_lot):
 
     conn = dbConnection()
     cursor = conn.cursor()
-    query = ("SELECT pklt_nm, lat, lot, ROUND(6371000 * acos(cos(radians(%s)) * cos(radians(lat)) * cos(radians(lot) - radians(%s)) + sin(radians(%s)) * sin(radians(lat)))) AS distance "
+    query = ("SELECT pklt_nm, lat, lot, bsc_prk_crg, bsc_prk_hr, add_prk_crg, add_prk_hr, day_max_crg, ROUND(6371000 * acos(cos(radians(%s)) * cos(radians(lat)) * cos(radians(lot) - radians(%s)) + sin(radians(%s)) * sin(radians(lat)))) AS distance "
              "FROM parking_info pi2 "
              "ORDER BY distance LIMIT 5")
     cursor.execute(query, (u_lat, u_lot, u_lat))
