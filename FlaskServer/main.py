7c2a4758eb93a579482a9a552fd9d1595941da19
Administrator <rudgurtn@naver.com>
2024-12-12 02:14:59 +0900

    24.12.11 - 경혁수 : 주차장 정보요청

--- FlaskServer/main.py
+++ FlaskServer/main.py
@@ -697,6 +697,13 @@ def insertHistory():
     except Exception as e:
         return jsonify({"success": False, "message": "서버 오류: " + str(e)}), 500
 
+@app.route('/realRemain', methods=['POST'])
+def realRemain():
+    try:
+        remain = realRemain()  # getParkInfo 호출
+        return jsonify({"success": True, "remain": remain}), 200
+    except Exception as e:
+        return jsonify({"success": False, "message": "서버 오류: " + str(e)}), 500
 
 # 이전 예측 소스
 
